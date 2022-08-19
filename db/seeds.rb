f7967f7da30b898ee9b29f101f937b9097c397af
1zach <zachjamesthomas@gmail.com>
2022-08-19 13:10:44 +0200

    Changed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'open-ur'
+require 'rest-client'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "getting"
+Movie.destroy_all
+movie = RestClient.get "http://tmdb.lewagon.com/movie/top_rated?"
+puts "parsing"
+puts "this is Movie" 
+movies_array = JSON.parse(movie)["results"]
+puts "this is array" 
+
+
+movies_array.each do |movie|
+    puts "create"
+    Movie.create(
+        title: movie['title'],
+        overview: movie['overview'],
+        poster_url: movie['poster_path'],
+        rating: movie['vote_average']
+    )
+    puts "created"
+end
